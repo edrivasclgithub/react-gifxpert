89ae88580a03a71cefd9afb351ef6d17d1badcd6
Eduardo Rivas C. <edrivasclgithub@gmail.com>
2025-03-20 20:11:16 -0400

    update

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -18,3 +18,38 @@ export const useFetchGifs = (category) => {
     isLoading,
   };
 };
+
+
+
+import { useState } from 'react';
+import { useCounter } from '../hooks';
+
+// const heavyStuff = (iterationNumber = 100) => {
+//   for (let i = 0; i < iterationNumber; i++) {
+//     console.log('ahi vamos...');
+//   }
+//   return `${iterationNumber} iteraciones realizadas`;
+// };
+
+export const MemoHook = () => {
+  const { counter, increment } = useCounter(10);
+  const [show, setShow] = useState(true);
+  return (
+    <>
+      <h1>
+        Counter: <small>{counter}</small>
+      </h1>
+      <hr />
+      {/* <h4>{heavyStuff(500)}</h4> */}
+      <button className="btn btn-primary" onClick={() => increment()}>
+        +1
+      </button>
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => setShow(!show)}
+      >
+        Show/Hide {JSON.stringify(show)}
+      </button>
+    </>
+  );
+};
